55690d9454c90d66d6f27386f6f013547c5f8d40
anjanai <anjanai.yahoo@gmail.com>
2023-05-03 19:26:19 -0700

    add more people

--- index.js
+++ index.js
@@ -5,44 +5,85 @@ var celeb_bank = [
 	  Mr Bean
 	  Richard Gere
 	  Virat Kohli
-	  Benjamin Bratt
+	  Colin Firth
+	  
 	  Amitabh (younger days)
-	  Sophia Loren	  
+	  Sophia Loren
+	  Brad Pitt
+	  Madhubala
+	  Shah Rukh Khan
+	  Shiv Kumar Sharma
+	  Denzel Washington
 	*/
 	"https://upload.wikimedia.org/wikipedia/commons/d/d3/Bill_Clinton.jpg",
 	"https://hips.hearstapps.com/hmg-prod/images/prince-charles-prince-of-wales-poses-for-an-official-news-photo-1573163200.jpg",
 	"https://images.theconversation.com/files/304864/original/file-20191203-67028-qfiw3k.jpeg?ixlib=rb-1.1.0&rect=638%2C2%2C795%2C745&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
 	"https://m.media-amazon.com/images/M/MV5BMTI2NDQ2OTY4M15BMl5BanBnXkFtZTYwNTYyNjc4._V1_FMjpg_UX1000_.jpg",
 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShm-Yo8ZoIK9bJPSjEW0Zo3ig9Mlk-ZOIW7g&usqp=CAU",
-	"https://static.tvtropes.org/pmwiki/pub/images/benjamin_bratt.jpg",
+	"https://upload.wikimedia.org/wikipedia/commons/d/d9/Flickr_-_nicogenin_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29_%2856%29_Colin_Firth.jpg",
+	
 	"https://i.ndtvimg.com/i/2015-05/amitabh-bachchan_640x480_51431434404.jpg",
 	"https://content.api.news/v3/images/bin/a99dc8298b33c91e4e4ce69873970d96",
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Brad_Pitt_2019_by_Glenn_Francis.jpg/1200px-Brad_Pitt_2019_by_Glenn_Francis.jpg",
+	"https://upload.wikimedia.org/wikipedia/commons/c/c4/Madhubala1957.png",
+	"https://upload.wikimedia.org/wikipedia/commons/6/6e/Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg",
+	"https://static.toiimg.com/thumb/msid-91466980,width-400,resizemode-4/91466980.jpg",
+	"https://e00-marca.uecdn.es/assets/multimedia/imagenes/2023/01/23/16744920354601.jpg",
+	
     ],
 
     
     [
 	/*
-	  Steve Jobs, Bill Gates
-	  Viv Richards
-	  Hritik Roshan
-	  Lewis Hamilton (F1)
-	  Hugh Grant
-	  Amitabh (older days)
-	  Julia Roberts
+	 Steve Jobs, Bill Gates
+	 Joey from Friends
+	 Cary Grant
+	 MS Dhoni
+	 Hugh Grant
+	 Amitabh (older days)
+	 Julia Roberts
 	*/
 	"https://cdn.mos.cms.futurecdn.net/3f8ff38c50a1ebb8045c180b7fce9b5b-1200-80.jpg",
 	"https://hai.stanford.edu/sites/default/files/styles/person_big/public/2020-03/billgatesheadshot.jpg?itok=6fmYlCKg",
-	"https://www.caribbeannationalweekly.com/wp-content/uploads/2022/05/VIV-RICHARDS.jpg",
-	"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Hrithik_at_Rado_launch.jpg/220px-Hrithik_at_Rado_launch.jpg",
-	"https://cdn-6.motorsport.com/images/mgl/0mb95oa2/s800/lewis-hamilton-mercedes-1.jpg",
+	"https://upload.wikimedia.org/wikipedia/en/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg",
+	
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Grant%2C_Cary_%28Suspicion%29_01_Crisco_edit.jpg/1200px-Grant%2C_Cary_%28Suspicion%29_01_Crisco_edit.jpg",
+	"https://upload.wikimedia.org/wikipedia/commons/f/f0/Dhoni1234.png",
 	"https://m.media-amazon.com/images/M/MV5BMTc4MTgxOTk2Ml5BMl5BanBnXkFtZTcwNzMwMjYwMw@@._V1_FMjpg_UX1000_.jpg",
 	"https://m.media-amazon.com/images/M/MV5BNTk1OTUxMzIzMV5BMl5BanBnXkFtZTcwMzMxMjI0Nw@@._V1_.jpg",
 	"https://cdn.britannica.com/01/236601-050-2CFDF711/Julia-Roberts-2019.jpg",
+
+	/*
+	  George Clooney
+	 Madhuri Dixit
+	 Hritik Roshan
+	 Pandit Jasraj
+	 Idris Alba
+	 */
+	"https://m.media-amazon.com/images/M/MV5BMjEyMTEyOTQ0MV5BMl5BanBnXkFtZTcwNzU3NTMzNw@@._V1_.jpg",
+	"https://static.toiimg.com/thumb/resizemode-4,width-1200,height-900,msid-94656030/94656030.jpg",
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Hrithik_at_Rado_launch.jpg/220px-Hrithik_at_Rado_launch.jpg",
+	"https://i.ytimg.com/vi/22mMRzcoaco/sddefault.jpg",
+	"https://upload.wikimedia.org/wikipedia/commons/0/0e/Idris_Elba-4580_%28cropped%29.jpg",
     ],
 
     
     [
 	/*
+	  Seinfeld, Jay Leno
+	  Indian Comedian Vir Das
+	  Harrison Ford
+	  Sachin Tendulkar
+	  Paul McCartney
+	  Rajesh Khanna
+	  Julie Andrews
+	  Tom Cruise
+	  Aishwarya Rai
+	  Aamir Khan
+	  Zakir Hussein
+	  Will Smith
+
+
 	  Seinfeld, Jay Leno
 	  Indian Comedian Vir Das
 	  Zakir Hussein
@@ -54,11 +95,20 @@ var celeb_bank = [
 	*/
 	"https://www.pbs.org/wnet/pioneers-of-television/files/2014/04/9088097831_d31af2d6e5_o.jpg",
 	"https://hips.hearstapps.com/hmg-prod/images/gettyimages-660314202.jpg",
-	"https://i.scdn.co/image/ab6761610000e5eb0b0764268407db8b3a218301",
-	"https://upload.wikimedia.org/wikipedia/en/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg",
+	"https://www.al.com/resizer/B2jAa-FYbveRmn4Hr1CZ6nGx1L8=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/YS2LGW42AVAMLEUQLQG4S5L7EA.jpg",
+	"https://static.wikia.nocookie.net/starwars/images/2/29/Harrisonford.jpg/revision/latest?cb=20190414125007",
+	"https://media.cnn.com/api/v1/images/stellar/prod/210327012507-sachin-tendulkar-file.jpg?q=w_1600,h_1065,x_0,y_0,c_fill",
 	"https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Paul_McCartney_2021_%28cropped%29.jpg/800px-Paul_McCartney_2021_%28cropped%29.jpg",
-	"https://cdn.dnaindia.com/sites/default/files/2023/04/27/2587080-anil-ambani.png?im=FitAndFill=(1200,900)",
+	"https://cdn.siasat.com/wp-content/uploads/2021/12/Rajesh-Khanna.jpg",
 	"https://hips.hearstapps.com/hmg-prod/images/julie-andrews-GettyImages-130044748_1600.jpg",
+
+	"https://flxt.tmsimg.com/assets/378_v9_bd.jpg",
+	"https://cdn.i-scmp.com/sites/default/files/d8/images/methode/2020/11/01/a8ff3d24-18d2-11eb-8f67-a484f6db61a1_image_hires_124711.jpg",
+	"https://upload.wikimedia.org/wikipedia/commons/0/0c/Aamir_Khan_%28Berlin_Film_Festival_2011%29.jpg",	
+	"https://i.scdn.co/image/ab6761610000e5eb0b0764268407db8b3a218301",
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/TechCrunch_Disrupt_2019_%2848834434641%29_%28cropped%29.jpg/640px-TechCrunch_Disrupt_2019_%2848834434641%29_%28cropped%29.jpg",
+	
+	
     ]
 ]
 
